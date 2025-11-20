5fa722cfa4f1039085b6259abc6abf0bb87b255e
Imranahmed935 <imrantahir9918@gmail.com>
2025-11-20 23:37:54 +0600

    utils added

---
+++ src/zod/specialties.validation.ts
@@ -0,0 +1,11 @@
+// import z from "zod";
+
+import z from "zod";
+
+// export const createSpecialityZodSchema = z.object({
+//     title: z.string().min(3, "Title must be at least 3 characters long"),
+// })
+
+export const createSpecialityZodSchema = z.object({
+    title:z.string().min(3, "Title must be at least 3 characters long")
+})
